9a421e7be12e33e11bdd68b39239595688a95278
Никита <jubin2la@mail.ru>
2020-02-27 23:55:55 +0300

    [add] UserAnswer isRight check

--- src/main/java/com/nyubin/service/UserAnswerServiceImpl.java
+++ src/main/java/com/nyubin/service/UserAnswerServiceImpl.java
@@ -1,5 +1,7 @@
 package com.nyubin.service;
 
+import com.nyubin.model.AnswerData;
+import com.nyubin.model.QuestionData;
 import com.nyubin.model.User;
 import com.nyubin.model.UserAnswer;
 import com.nyubin.repository.QuestionDataRepo;
@@ -31,14 +33,26 @@ public class UserAnswerServiceImpl implements UserAnswerService {
 
     @Override
     public UserAnswer save(UserAnswer newUserAnswer, User user) {
-        newUserAnswer.setUserId(user.getId());
 
+        newUserAnswer.setUserId(user.getId());
         //TODO нужна ли эта проверка
 
-//        if(questionDataRepo.findAllIds().contains(newUserAnswer.getId())){
-//
-//        }
+        if(!questionDataRepo.findAllIds().contains(newUserAnswer.getQuestionId())){
+            System.out.println("Всё плохо!");
+        }
+        //TODO обработать optional
+        Optional<QuestionData> byId = questionDataRepo.findById(newUserAnswer.getQuestionId());
 
+        for (AnswerData answerData:byId.get().getAnswerDataSet()
+             ) {
+            if (Boolean.TRUE.equals(answerData.isRight())){
+                if (newUserAnswer.getUserAnswer().toLowerCase().equals(answerData.getName())){
+                    newUserAnswer.setRight(Boolean.TRUE);
+                }else{
+                    newUserAnswer.setRight(Boolean.FALSE);
+                }
+            }
+        }
         return userAnswerRepo.save(newUserAnswer);
 
     }
